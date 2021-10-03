5fffd73f9771445919abb2f943aa9e8e2d5439e4
Felipe <Felipe.debene@gmail.com>
2021-10-02 21:24:46 -0500

    More files

---
+++ functions.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
+function revoke_and_auth_new_ip {
+  #Revoke Previous IP
+  echo "Revoking old ip"
+  aws ec2 revoke-security-group-ingress --group-id $1 \
+    --ip-permissions \
+    "`aws ec2 describe-security-groups --output json --group-ids $1 --query "SecurityGroups[0].IpPermissions"`"
+
+  #Authorize New IP
+  echo "Authorizing new ip"
+  aws ec2 authorize-security-group-ingress --group-id $1 --cidr $2 --protocol udp --port 51820
+
+}
+
+
+function clean_env_vars {
+  unset AWS_ACCESS_KEY_ID
+  unset AWS_SECRET_ACCESS_KEY
+  unset AWS_SESSION_TOKEN
+  unset current_ip
+  unset sg_ip
+  unset groupId
+}
+
+
+function clean_tmp {
+  rm -r /tmp/curIp
+  rm -r /tmp/tempCred
+}
